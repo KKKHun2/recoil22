a58bf6f5f16d9b3e212ed9a69aaec15456a7a5e8
KKKHun2 <1224kim2son@naver.com>
2023-08-22 16:43:38 +0900

    ✨useLocation 이용해서 현재 페이지 확인

--- src/components/Navi/Navbar.tsx
+++ src/components/Navi/Navbar.tsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 import { Logo } from "./../Logo";
 import McLogo from "../../assets/McLogo.jpg";
 
 function Navbar() {
+  const location = useLocation(); // 현재 경로 가져오기
+
   return (
     <Header>
       <Wrapper>
@@ -11,8 +14,12 @@ function Navbar() {
           <Logo src={McLogo} alt="홈 아이콘" />
         </Link>
         <Nav>
-          <Link to={"/"} aria-label="홈으로 이동">홈</Link>
-          <Link to={"/cart"} aria-label="장바구니로 이동">장바구니</Link>
+          <StyledLink to={"/"} isActive={location.pathname === "/"} aria-label="홈으로 이동">
+            홈
+          </StyledLink>
+          <StyledLink to={"/cart"} isActive={location.pathname === "/cart"} aria-label="장바구니로 이동">
+            장바구니
+          </StyledLink>
         </Nav>
       </Wrapper>
     </Header>
@@ -28,6 +35,7 @@ const Header = styled.header`
   z-index: 9999;
   background-color: #fff;
 `;
+
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -38,16 +46,20 @@ const Wrapper = styled.div`
   justify-content: space-between;
   margin: 0 auto;
 `;
+
 const Nav = styled.nav`
   max-width: 1024px;
   display: flex;
   justify-content: right;
   gap: 36px;
   align-items: center;
-  > span {
-    font-weight: bold;
-    cursor: pointer;
-  }
-  `;
-  
+`;
+
+const StyledLink = styled(Link)<{ isActive: boolean }>`
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  color: ${({ isActive }) => (isActive ? "red" : "black")};
+`;
+
 export default Navbar;
