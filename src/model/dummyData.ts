f16057cb69c3318c2da7e8ee0511e6c79b48d0a8
KKKHun2 <1224kim2son@naver.com>
2023-08-22 16:36:03 +0900

    📝 더미데이터 내용수정

--- src/model/dummyData.ts
+++ src/model/dummyData.ts
@@ -1,81 +1,76 @@
-export interface ICartItem {
-    id: number;
-    price: number;
-    title: string;
-    description: string;
-  }
+import {CartAItem} from "../recoil/CartAtom";
   
-  export const dummyData: ICartItem[] = [
+  export const dummyData: CartAItem[] = [
     {
       id: 1,
       price: 2000,
-      title: "Product A",
+      title: "불고기버거",
       description: "This is the first product.",
     },
     {
       id: 2,
       price: 1500,
-      title: "Product B",
+      title: "새우버거",
       description: "An affordable second product.",
     },
     {
       id: 3,
       price: 3000,
-      title: "Product C",
+      title: "더블불고기버거",
       description: "A premium third product.",
     },
     {
       id: 4,
-      price: 1800,
-      title: "Product D",
+      price: 4800,
+      title: "상하이치킨버거",
       description: "A unique fourth product.",
     },
     {
       id: 5,
-      price: 2500,
-      title: "Product E",
+      price: 1000,
+      title: "맥너겟",
       description: "Fifth product with a special feature.",
     },
     {
       id: 6,
       price: 1200,
-      title: "Product F",
+      title: "감자튀김",
       description: "A budget-friendly sixth product.",
     },
     {
       id: 7,
       price: 2800,
-      title: "Product G",
+      title: "콜라",
       description: "Seventh product designed for excellence.",
     },
     {
       id: 8,
       price: 2200,
-      title: "Product H",
+      title: "사이다",
       description: "Eighth product for everyday use.",
     },
     {
       id: 9,
       price: 1700,
-      title: "Product I",
+      title: "샐러드",
       description: "Ninth product with innovative design.",
     },
     {
       id: 10,
       price: 1900,
-      title: "Product J",
+      title: "콘샐러드",
       description: "Tenth product for a unique experience.",
     },
     {
         id: 11,
         price: 2200,
-        title: "한국말",
+        title: "빅새우버거",
         description: "안녕안녕.",
       },
       {
         id: 12,
         price: 1900,
-        title: "한국2",
+        title: "1977버거",
         description: "12개까지는 있어야지!",
       },
   ];
